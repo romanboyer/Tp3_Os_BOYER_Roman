8015fbbacd39ce7aba66301a691175e430f168f3
Roman Boyer <romanboyer.pro@gmail.com>
2026-04-21 11:55:15 +0200

    Rename message functions for consistency

--- creme.h
+++ creme.h
@@ -7,14 +7,14 @@ int beuip_start(const char *pseudo);
 /* Arrête le serveur proprement */
 int beuip_stop(void);
 
-/* Envoie la commande liste au serveur local */
-int mess_liste(void);
+/* Affiche la liste des connectés */
+int beuip_list(void);
 
 /* Envoie un message à un pseudo précis */
-int mess_send_one(const char *pseudo, const char *message);
+int beuip_message_one(const char *pseudo, const char *message);
 
 /* Envoie un message à tous les connectés */
-int mess_send_all(const char *message);
+int beuip_message_all(const char *message);
 
 /* Demande la liste des fichiers à un pseudo */
 int beuip_ls(const char *pseudo);
